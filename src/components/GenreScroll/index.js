09993c2ddaa590be18f3b526f7ab28cb8ffd83c3
nerocodes <neropaulej@gmail.com>
2019-08-29 20:36:30 +0100

    feature(homepage): created slider component

---
+++ src/components/GenreScroll/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './genreScroll.scss';
+
+const GenreSlider = () => (
+  <section className="genre-scroll">
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+    <div className="genre">
+      <span className="name">Action</span>
+    </div>
+  </section>
+);
+
+export default GenreSlider;
