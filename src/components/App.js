d743faaefcc2e097cd966d2a7b0483530601b1ae
Bella Oyedele <delebella@yahoo.com>
2019-08-29 10:46:19 +0100

    feature(landingPage): landing page
    Create Footer Component
    Create other component for landing page
    Create styling
    
    [Finishes #167164932]

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HelloDahlia from './DummyComponent/DummyComponent';
+import LandingPageComponent from './LandingPageComponent/LandingPage';
+import FooterComponent from './FooterComponent/Footer';
 import style from './app.scss';
 
 const Welcome = () => (
   <div className={style.app}>
-    <h1>Authors Haven</h1>
-    <div className={style.sassyDiv}>Welcome!</div>
+    <LandingPageComponent />
+    <FooterComponent />
   </div>
 );
 
