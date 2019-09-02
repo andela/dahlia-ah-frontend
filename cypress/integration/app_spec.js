b18c380b7fe94aa7d0679da2b6bfa95aae29e4f1
Bella Oyedele <delebella@yahoo.com>
2019-09-02 18:03:27 +0100

    feature(notFound) add not found page
    create not found component
    add styling
    add unit and end to end test
    
    [Finishes #168248608]

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -12,4 +12,11 @@ describe('Authors Haven Frontend Pages', () => {
       .find('h2')
       .should('have.text', 'Membership');
   });
+
+  it('should show 404 error', () => {
+    cy.visit('/jinjbnjnjnnkjlo');
+    cy.get('.notFound')
+      .find('h1')
+      .should('have.text', '404');
+  });
 });
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from '../layout/Navbar';
 import Footer from '../layout/Footer/Footer';
 import LandingPage from '../LandingPage/LandingPage';
+import NotFound from '../NotFound/NotFound';
 
 const Router = () => (
   <div>
     <Navbar />
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route component={NotFound} />
     </Switch>
     <Footer />
   </div>
