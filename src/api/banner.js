d1831486f3d690e89bf694ca9aa7e7d4496fee75
Bella Oyedele <delebella@yahoo.com>
2019-09-06 08:06:39 +0100

    remove console log

--- src/api/banner.js
+++ src/api/banner.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import appConfig from '../config/appConfig';
 
 const { BACKEND_PATH } = appConfig;
-console.log(BACKEND_PATH);
 export default (limit = 1, setBanner) => {
   axios.get(`${BACKEND_PATH}/novels/random?limit=${limit}`)
     .then(({ data }) => {
