ebad1a329201767ada31006b5514cc3b17387231
Oseikweme Benjamin Isidahomen <oseikwemeisidahomen@gmail.com>
2019-09-12 13:21:52 +0100

    feature(settings): opt in or out of notification
     - modify style of settings
    
    [Finishes #167164955]

--- src/components/SettingsItem/SettingsItem.jsx
+++ src/components/SettingsItem/SettingsItem.jsx
@@ -10,13 +10,13 @@ const SettingsItem = ({ initialState, title, description }) => {
   return (
     <div className="settings-item">
       <div className="settings-action">
-        <p>{title}</p>
+        <p className="title">{title}</p>
         <div className="toggle-btn">
           <span id="on" className={selected === 'ON' ? 'selected' : ''} role="presentation" onClick={() => handleSelected('ON', setSelected)}>ON</span>
           <span id="off" className={selected === 'OFF' ? 'selected' : ''} role="presentation" onClick={() => handleSelected('OFF', setSelected)}>OFF</span>
         </div>
       </div>
-      <p>{description}</p>
+      <p className="description">{description}</p>
     </div>
   );
 };
